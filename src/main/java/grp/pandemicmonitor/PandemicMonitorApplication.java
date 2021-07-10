215e2fa0b357746a6e75c6e059f5461d3afe2d6d
pjy <junyu200019@hotmail.com>
2021-07-10 10:18:49 +0800

    创建工程

---
+++ src/main/java/grp/pandemicmonitor/PandemicMonitorApplication.java
@@ -0,0 +1,13 @@
+package grp.pandemicmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandemicMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PandemicMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/grp/pandemicmonitor/PandemicMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package grp.pandemicmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandemicMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
