c543921d1dbfd30c48c4e120337f5a651139db8c
JohnsonKHOO <khoozhongsheng333@gmail.com>
2021-07-10 16:41:57 +0800

    添加config类解决跨域问题（注释掉了）和application.yml连接mysql配置

--- src/main/java/grp/pandemicmonitor/config/CrosConfig.java
+++ src/main/java/grp/pandemicmonitor/config/CrosConfig.java
@@ -1,19 +1,19 @@
-package grp.pandemicmonitor.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-
-public class CrosConfig implements WebMvcConfigurer{
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET","HEAD","POST","DELETE","OPTIONS","PUT")
-                .allowCredentials(true)
-                .maxAge(3600)
-                .allowedHeaders("*");
-    }
-}
+//package grp.pandemicmonitor.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//
+//public class CrosConfig implements WebMvcConfigurer{
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*")
+//                .allowedMethods("GET","HEAD","POST","DELETE","OPTIONS","PUT")
+//                .allowCredentials(true)
+//                .maxAge(3600)
+//                .allowedHeaders("*");
+//    }
+//}
