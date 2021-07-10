4fedc7285d0562cd57fa08717463e3052e0b819d
JohnsonKHOO <khoozhongsheng333@gmail.com>
2021-07-10 16:38:13 +0800

    添加config类解决跨域问题和application.yml连接mysql配置

--- src/main/java/grp/pandemicmonitor/controller/PanController.java
+++ src/main/java/grp/pandemicmonitor/controller/PanController.java
@@ -2,12 +2,14 @@ package grp.pandemicmonitor.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/PandamicMonitor")
-public class PanController {
 
-    public String say(String message){
+public class PanController {
+    @RequestMapping(value ="/PandamicMonitor")
+
+    public @ResponseBody String say(String message){
         return "hello" + message;
     }
 
