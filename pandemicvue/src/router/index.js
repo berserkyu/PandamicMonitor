4a463c5fc9782167446e05f2ba9f4ecd52510d07
JohnsonKHOO <khoozhongsheng333@gmail.com>
2021-07-12 14:08:45 +0800

    前端登录页面Axios通信

--- pandemicvue/src/router/index.js
+++ pandemicvue/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+import Home from '../components/home/home.vue'
+import HelloWorld from '../components/HelloWorld.vue'
 
 Vue.use(VueRouter)
 
@@ -12,6 +14,14 @@ Vue.use(VueRouter)
     {
       path: "/login",
       component: Login
+    },
+    {
+      path: "/home",
+      component: Home
+    },
+    {
+      path: "/helloworld",
+      component: HelloWorld
     }
   ]
 
