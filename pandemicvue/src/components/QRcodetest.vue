baf41024460c7dd372d965db02d72864f13b6c13
pjy <junyu200019@hotmail.com>
2021-07-16 20:49:35 +0800

    生成/扫描二维码

---
+++ pandemicvue/src/components/QRcodetest.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- 这是生成二维码的代码
+    <qr-code text="PandemicMonitorLoc1000"></qr-code>
+    -->
+    <!--扫描二维码-->
+    <qrcode-stream @decode="onDecode"></qrcode-stream>
+  </div>
+
+
+</template>
+<script>
+
+export default {
+  methods:{
+    onDecode (decodedString) {
+      console.log(decodedString);
+    }
+  }
+
+}
+
+</script>
