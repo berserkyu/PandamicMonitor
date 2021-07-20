c5f49b9de6ad6fb3deffa82915e1da6af59c6366
pjy <junyu200019@hotmail.com>
2021-07-20 09:06:03 +0800

    用户列表页面测试+调整

--- pandemicvue/src/components/UserList.vue
+++ pandemicvue/src/components/UserList.vue
@@ -13,14 +13,14 @@
     </el-form>
     <!--用户信息列表-->
     <el-main>
-      <el-table :data="tableData" v-for="user_info in tableData" highlight-current-row border style="width: 100%;" class="userlist_table" height="500">
-        <el-table-column prop="name" label="姓名"></el-table-column>
-        <el-table-column prop="id" label="证件号"></el-table-column>
-        <el-table-column prop="email" label="电子邮箱"></el-table-column>
+      <el-table :data="tableData"  highlight-current-row border style="width: 100%;" class="userlist_table" height="500">
+        <el-table-column prop="fullName" label="姓名"></el-table-column>
+        <el-table-column prop="idNo" label="证件号"></el-table-column>
+        <el-table-column prop="mail" label="电子邮箱"></el-table-column>
         <el-table-column label="操作" width="180" align="center">
           <template slot-scope="scope">
             <el-button type="text" icon="el-icon-edit" @click="edit(scope.row)">编辑</el-button>
-            <el-button type="text" icon="el-icon-delete" class="deleteColor" @click="del(scope.row.id)">删除</el-button>
+            <el-button type="text" icon="el-icon-delete" class="deleteColor" @click="del(scope.row.idNo)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -66,19 +66,19 @@
         <el-form label-width="100px" :model="editForm" :rules="editRules" ref="editForm" class="edit_Form">
           <!--姓名输入框-->
           <el-form-item label="姓名：" prop="name">
-            <el-input v-model="editForm.name" placeholder="请输入用户的真实姓名" clearable></el-input>
+            <el-input v-model="editForm.fullName" placeholder="请输入用户的真实姓名" clearable></el-input>
           </el-form-item>
           <!--邮箱输入框-->
           <el-form-item label="电子邮箱：" prop="email">
-            <el-input v-model="editForm.email" placeholder="请输入用户的电子邮箱" clearable></el-input>
+            <el-input v-model="editForm.mail" placeholder="请输入用户的电子邮箱" clearable></el-input>
           </el-form-item>
           <!--地址输入框-->
           <el-form-item label="地址" prop="address">
-            <el-input v-model="editForm.password" placeholder="请输入用户的家庭地址" clearable show-password></el-input>
+            <el-input v-model="editForm.address" placeholder="请输入用户的家庭地址" clearable ></el-input>
           </el-form-item>
           <!--联系方式输入框-->
           <el-form-item label="联系方式" prop="phonenumber">
-            <el-input v-model="editForm.phonenumber" placeholder="请输入用户的手机号码/电话号码" clearable></el-input>
+            <el-input v-model="editForm.phoneNo" placeholder="请输入用户的手机号码/电话号码" clearable></el-input>
           </el-form-item>
         </el-form>
         <!--编辑用户信息对话框按钮-->
@@ -92,225 +92,218 @@
 </template>
 
 <script>
-  export default{
-    name:'UserList',
-    data(){
-      return{
-        tableData: [{ //表格信息测验
-          name: '俊宇',
-          id: '000001',
-          email: '000001@qq.com'
-        }, {
-          name: '宗胜',
-          id: '000002',
-          email: '000002@qq.com'
-        }, {
-          name: '倩茹',
-          id: '000003',
-          email: '000003@qq.com'
-        }, {
-          name: '浩廷',
-          id: '000004',
-          email: '000004@qq.com'
-        },{
-          name: '思宇',
-          id: '000005',
-          email: '000005@qq.com'
-        }],
-        addFormVisible: false,
-        editFormVisible: false,
-        userlistForm:{
-          name:''
-        },
-        user_info: [],
-        addForm:{
-          name:'',
-          idtype:'',
-          idno:'',
-          email:'',
-          password:''
-        },
-        editForm:{
-          name:'',
-          email:'',
-          address:'',
-          phonenumber:''
-        },
-        /*Form组件提供的表单验证功能，通过rules属性传入约定的验证规则，并将Form-Item的prop属性设置为需校验的字段名*/
-        /*新增用户信息表单验证*/
-        addRules:{
-          name:[
-            {required:true, message:'请输入用户的真实姓名', trigger:'blur'}
-          ],
-            idtype:[
-            {required:true, message:'请选择用户的证件类型', trigger:'change'}
-          ],
-            idno:[
-            {required:true, message:'请输入用户的证件号', trigger:'blur'}
-          ],
-            email:[
-            {required:true, message:'请输入用户的电子邮箱', trigger:'blur'},
-            {type:'email', message:'请输入正确的邮箱地址', trigger:['blur','change']}
-          ],
-            password:[
-            {required:true, message:'请输入密码', trigger:'blur'},
-            {min:6, max:15, message:'长度在6到15个字符', trigger:'blur'}
-          ],
-        },
-        /*编辑用户信息表单验证*/
-        editRules:{
-          name:[
-            {required:true, message:'请输入用户的真实姓名', trigger:'blur'}
-          ],
-          email:[
-            {required:true, message:'请输入用户的电子邮箱', trigger:'blur'},
-            {type:'email', message:'请输入正确的邮箱地址', trigger:['blur','change']}
-          ],
-          address:[
-            {required:true, message:'请输入用户的家庭地址', trigger:'blur'},
-            {min:10, max:50, message:'长度在10到50个字符', trigger:'blur'}
-          ],
-          phonenumber:[
-            {required:true, message:'请输入用户的电话号码/手机号码', trigger:'blur'},
-            {min:1, max:15, message:'长度在1到15个字符', trigger:'blur'}
-          ],
-        }
+var oldmail;
+export default{
+  mounted() {
+    this.getdata();
+  },
+  name:'UserList',
+  data(){
+    return{
+      tableData:[],
+      addFormVisible: false,
+      editFormVisible: false,
+      userlistForm:{
+        name:''
+      },
+      user_info: [],
+      addForm:{
+        name:'',
+        idtype:'',
+        idno:'',
+        email:'',
+        password:''
+      },
+      editForm:{
+        fullName:'',
+        mail:'',
+        address:'',
+        phoneNo:''
+      },
+      /*Form组件提供的表单验证功能，通过rules属性传入约定的验证规则，并将Form-Item的prop属性设置为需校验的字段名*/
+      /*新增用户信息表单验证*/
+      addRules:{
+        name:[
+          {required:true, message:'请输入用户的真实姓名', trigger:'blur'}
+        ],
+        idtype:[
+          {required:true, message:'请选择用户的证件类型', trigger:'change'}
+        ],
+        idno:[
+          {required:true, message:'请输入用户的证件号', trigger:'blur'}
+        ],
+        email:[
+          {required:true, message:'请输入用户的电子邮箱', trigger:'blur'},
+          {type:'email', message:'请输入正确的邮箱地址', trigger:['blur','change']}
+        ],
+        password:[
+          {required:true, message:'请输入密码', trigger:'blur'},
+          {min:6, max:15, message:'长度在6到15个字符', trigger:'blur'}
+        ],
+      },
+      /*编辑用户信息表单验证*/
+      editRules:{
+        name:[
+          {required:true, message:'请输入用户的真实姓名', trigger:'blur'}
+        ],
+        email:[
+          {required:true, message:'请输入用户的电子邮箱', trigger:'blur'},
+          {type:'email', message:'请输入正确的邮箱地址', trigger:['blur','change']}
+        ],
+        address:[
+          {required:true, message:'请输入用户的家庭地址', trigger:'blur'},
+          {min:10, max:50, message:'长度在10到50个字符', trigger:'blur'}
+        ],
+        phonenumber:[
+          {required:true, message:'请输入用户的电话号码/手机号码', trigger:'blur'},
+          {min:1, max:15, message:'长度在1到15个字符', trigger:'blur'}
+        ],
       }
+    }
+  },
+  methods:{
+    search(){ //搜索按钮方法
+      console.log("get all data");
+      this.$axios
+        .post('getuserbyname',{
+          fullName: this.userlistForm.name
+        })
+        .then(successResponse => {
+          console.log("success");
+          this.tableData = successResponse.data.tableData;
+        }) .catch(failResponse =>{alert("跨域操作失败！")})
     },
-    methods:{
-      search(){ //搜索按钮方法
-        this.getdata(this.userlistForm)
-      },
-      add(){ //新增按钮方法
-        this.addForm = {};
-        this.addFormVisible = true;
-      },
-      save() { //新增用户表单的保存
-        this.$confirm('是否确定新增此用户？', '提示', {
-          confirmButtonText: '确定', //弹出框的确定提交按钮
-          cancelButtonText: '取消', //弹出框的取消提交按钮
-          type: 'warning', //弹出框类型
-          center: true
-        }).then(() => { //按下确定弹出消息
+    add(){ //显示新增用户表单
+      this.addForm = {};
+      this.addFormVisible = true;
+    },
+    save() { //新增用户表单的保存
+      this.$confirm('是否确定新增此用户？', '提示', {
+        confirmButtonText: '确定', //弹出框的确定提交按钮
+        cancelButtonText: '取消', //弹出框的取消提交按钮
+        type: 'warning', //弹出框类型
+        center: true
+      }).then(() => { //按下确定弹出消息
 
-          this.$axios
-            .post('', {
-              name: this.addForm.name,
-              idtype: this.addForm.idtype,
-              id: this.addForm.idno,
-              mail: this.addForm.email,
-              password: this.addForm.password
-            })
-            .then(successResponse => {
-              if (successResponse.data.code === 200) {
-                this.$message({
-                  type: 'success',
-                  message: '新增用户成功！'
-                });
-              } else {
-                this.$message({
-                  type: 'error',
-                  message: '新增用户失败'
-                });
-              }
-            }) .catch(failResponse =>{alert("跨域操作失败！")})
-
-        })
-      },
-      save2() { //编辑用户信息表单的保存
-        this.$confirm('是否确定编辑此用户信息？', '提示', {
-          confirmButtonText: '确定', //弹出框的确定提交按钮
-          cancelButtonText: '取消', //弹出框的取消提交按钮
-          type: 'warning', //弹出框类型
-          center: true
-        }).then(() => { //按下确定弹出消息
-
-          this.$axios
-            .post('', {
-              name: this.editForm.name,
-              mail: this.editForm.email,
-              address: this.editForm.address,
-              phoneno: this.editForm.phonenumber
-            })
-            .then(successResponse => {
-              if (successResponse.data.code === 200) {
-                this.$message({
-                  type: 'success',
-                  message: '新增用户成功！'
-                });
-              } else {
-                this.$message({
-                  type: 'error',
-                  message: '新增用户失败'
-                });
-              }
-            }) .catch(failResponse =>{alert("跨域操作失败！")})
-
-        })
-      },
-      edit(obj){ //编辑按钮方法
-        this.editForm = obj;
-        this.editFormVisible = true;
-      },
-      del() { //删除按钮方法
-        this.$confirm('是否确定删除此用户？', '提示', {
-          confirmButtonText: '确定', //弹出框的确定提交按钮
-          cancelButtonText: '取消', //弹出框的取消提交按钮
-          type: 'warning', //弹出框类型
-          center: true
-        }).then(() => { //按下确定弹出消息
-
-          this.$axios
-            .post('', {
-              id: this.user_info.id
-            })
-            .then(successResponse => {
-              if (successResponse.data.code === 200) {
-                this.$message({
-                  type: 'success',
-                  message: '删除用户成功！'
-                });
-              } else {
-                this.$message({
-                  type: 'error',
-                  message: '删除用户失败'
-                });
-              }
-            }) .catch(failResponse =>{alert("跨域操作失败！")})
-        })
-      },
-      getdata() {
         this.$axios
-          .post('', {
-            id: this.user_info.id,
-            name: this.user_info.name,
-            mail: this.user_info.mail
+          .post('register', {
+            fullname: this.addForm.name,
+            idtype: this.addForm.idtype,
+            idNo: this.addForm.idno,
+            mail: this.addForm.email,
+            password: this.addForm.password
           })
           .then(successResponse => {
             if (successResponse.data.code === 200) {
-              this.tableData = successResponse.data.tableData;
+              this.$message({
+                type: 'success',
+                message: '新增用户成功！'
+              });
+            } else {
+              this.$message({
+                type: 'error',
+                message: '新增用户失败'
+              });
             }
           }) .catch(failResponse =>{alert("跨域操作失败！")})
-      }
+
+      })
+    },
+    save2() { //编辑用户信息表单的保存
+      this.$confirm('是否确定编辑此用户信息？', '提示', {
+        confirmButtonText: '确定', //弹出框的确定提交按钮
+        cancelButtonText: '取消', //弹出框的取消提交按钮
+        type: 'warning', //弹出框类型
+        center: true
+      }).then(() => { //按下确定弹出消息
+
+        this.$axios
+          .post('/changeuserinfo', {
+            name: this.editForm.fullName,
+            oldMail: oldmail,
+            newMail: this.editForm.mail,
+            address: this.editForm.address,
+            phoneno: this.editForm.phoneNo
+          })
+          .then(successResponse => {
+            if (successResponse.data.code == 200) {
+              this.$message({
+                type: 'success',
+                message: '编辑用户信息成功！'
+              });
+            } else {
+              this.$message({
+                type: 'error',
+                message: '编辑用户信息失败'
+              });
+            }
+          }) .catch(failResponse =>{alert("跨域操作失败！")})
+
+      })
+    },
+    edit(obj){ //编辑按钮方法
+      console.log(obj);
+      this.editForm = obj;
+      this.editFormVisible = true;
+      this.editForm.email =obj.mail;
+      oldmail = obj.mail;
+    },
+    del(idNoParam) { //删除按钮方法
+      this.$confirm('是否确定删除此用户？', '提示', {
+        confirmButtonText: '确定', //弹出框的确定提交按钮
+        cancelButtonText: '取消', //弹出框的取消提交按钮
+        type: 'warning', //弹出框类型
+        center: true
+      }).then(() => { //按下确定弹出消息
+        console.log(idNoParam);
+        this.$axios
+          .post('deleteuser', {
+            idNo: idNoParam
+          })
+          .then(successResponse => {
+            if (successResponse.data.code === 200) {
+              this.$message({
+                type: 'success',
+                message: '删除用户成功！'
+              });
+              this.getdata();
+            } else {
+              this.$message({
+                type: 'error',
+                message: '删除用户失败'
+              });
+            }
+          }) .catch(failResponse =>{alert("跨域操作失败！")})
+      })
+    },
+    getdata() {
+      console.log("get all data");
+      this.$axios
+        .get('/getalluser')
+        .then(successResponse => {
+          console.log("success");
+          this.tableData = successResponse.data.tableData;
+        }) .catch(failResponse =>{alert("跨域操作失败！")})
     }
   }
+}
 
 </script>
 
 <style scoped>
-  /*根节点样式*/
-  .userlist_container{
-    height:100%;
-    width:100%;
-  }
+/*根节点样式*/
+.userlist_container{
+  height:100%;
+  width:100%;
+}
 
-  .user_search{
-    margin-top:20px;
-    margin-left: 15px;
-    padding: 0 10px;
-  }
+.user_search{
+  margin-top:20px;
+  margin-left: 15px;
+  padding: 0 10px;
+}
 
-  .deleteColor{
-    color:red;
-  }
+.deleteColor{
+  color:red;
+}
 </style>
